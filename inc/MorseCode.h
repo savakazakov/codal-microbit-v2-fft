df1b346931b49a0e0e8789c82fd500a3987d7292
Joshua Hill <j.hill8@lancaster.ac.uk>
2021-05-21 19:41:55 +0100

    [morse] added ways to get data out

--- inc/MorseCode.h
+++ inc/MorseCode.h
@@ -1,18 +1,14 @@
 /*
 The MIT License (MIT)
-
 Copyright (c) 2021 Lancaster University.
-
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation
 the rights to use, copy, modify, merge, publish, distribute, sublicense,
 and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -97,9 +93,8 @@ class MorseCode : public DataSink
     void playChar(char c, bool primary);
     void playString(std::string c, bool primary);
     void serialPrintStored(bool primary);
-    void printStored(char word[WORD_LEN], int wordPos);
+    char getLastChar(bool primary);
+    void clearStored(bool primary, int& wordPos);
+    std::string getStored(bool primary);
 };
-
-
-
 #endif
